c077d97b0d482051c3f0670e7e8ba5cdf5ee82a1
Maciej Sienkiewicz <maciej@macoscope.com>
2019-12-29 22:46:04 +0100

    Removed jsonq

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import { getData } from './data';
 
-var jsonQ = require('jsonq');
-
 let dataDiv = document.getElementById("data");
 dataDiv.textContent = "Loading";
 var loader = setInterval(function() { dataDiv.textContent += "." }, 1000);
